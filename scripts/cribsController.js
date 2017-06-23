3cbf3b7d7f790acf4d5f95c18bc1be114ee52bfc
Matt Sullivan <mattbsullivan@gmail.com>
2017-06-23 11:23:22 -0700

    edit and delete functionality implemented

--- scripts/cribsController.js
+++ scripts/cribsController.js
@@ -17,6 +17,23 @@ angular
         $scope.newListing = {};
       }
 
+      $scope.editCrib = function(crib) {
+        $scope.editListing = true;
+        $scope.existingListing = crib;
+      }
+
+      $scope.saveCribEdit = function() {
+        $scope.existingListing = {};
+        $scope.editListing = false;
+      }
+
+      $scope.deleteCrib = function(listing) {
+        var index = $scope.cribs.indexOf(listing);
+        $scope.cribs.splice(index, 1);
+        $scope.existingListing = {};
+        $scope.editListing = false;
+      }
+
       cribsFactory.getCribs().success(function(data) {
         $scope.cribs = data;
       }).error(function(error) {
